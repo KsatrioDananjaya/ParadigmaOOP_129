d5661e43a55909478b6df3e407dbdf2300d3e366
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-05-22 15:33:50 +0700

    membuat hpp anak

---
+++ mainIA/anak.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada \n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
