40bab1885c76be2dc1d3a10bf1e64898de06df3c
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-05-22 15:35:45 +0700

    membuat hpp ibu dan memperbaiki hpp anak

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -3,6 +3,7 @@
 #define ANAK_H
 
 class anak {
+public:
 	string nama;
 	anak(string pNama) :nama(pNama) {
 		cout << "Anak \"" << nama << "\" ada \n";
---
+++ mainIA/ibu.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+
+};
+#endif // !IBU_H
